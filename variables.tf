2fb48b362dafc6afe3c5c09c29496c7f66b3d515
acdmAnderson <acdm.anderson@hotmail.com>
2024-10-09 22:20:56 -0300

    feat: adding new variables

--- variables.tf
+++ variables.tf
@@ -5,13 +5,17 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
-
-variable "role" {
-  description = "Academy role"
-  default     = "arn:aws:iam::245903023069:role/LabRole"
-}
-
 variable "cluster_name" {
   description = "Cluster name"
   default     = "tech-challenge-73"
+}
+
+variable "account_id" {
+  type = string
+  description = "AWS Academy account id"
+}
+
+variable "default_security_group" {
+  type = string
+  description = "AWS Academy default security group"
 }
